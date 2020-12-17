625caa3a53025aca9f3449afab957771aef00937
abhay-tank <abhay.tank@raweng.com>
2020-12-18 04:16:17 +0530

    Implemented basic file I/O

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const multer = require("multer");
-
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
 const app = express();
-const upload = multer({ storage: "uploads/" });
+dotenv.config({ path: path.join(__dirname, "config.env") });
 
-app.post("/", upload.single("proficPicture"), (req, res) => {
-	console.log(req.file);
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		const userDirectory = path.join(
+			__dirname,
+			process.env.PICTURE_CDN,
+			req.body.uid
+		);
+		const dirExists = fs.existsSync(userDirectory);
+		if (!dirExists) {
+			fs.mkdirSync(userDirectory);
+		}
+		return cb(null, userDirectory);
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + file.originalname);
+	},
 });
+const upload = multer({ storage, limits: { fileSize: 1024 * 1024 * 10 } });
+
+app.use(express.json());
+app.post("/", upload.single("profilePicture"), (req, res) => {
+	console.log(req.body.name);
+	console.log(req.file);
+	const fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+	let picturePath = req.body.uid + "/" + req.file.filename;
+	res.send(fullUrl + picturePath);
+});
+
+app.use(express.static(process.env.PICTURE_CDN));
 
 app.listen(3000, () => {
 	console.log(`Server running on http://localhost:3000`);
